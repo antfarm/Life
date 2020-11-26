51d2dd535f709bc5fa57929ffc44aada6d6dbd8b
Sean Buttinger <s.buttinger@tinkerbots.com>
2020-11-26 10:03:56 +0100

    Initial Commit

---
+++ Life/LifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeApp.swift
+//  Life
+//
+//  Created by sean on 26.11.20.
+//
+
+import SwiftUI
+
+@main
+struct LifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
