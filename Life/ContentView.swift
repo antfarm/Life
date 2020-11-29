8071aab76b627157da0f3f16cc95e43d1c90c35e
Sean Buttinger <s.buttinger@tinkerbots.com>
2020-11-29 13:55:48 +0100

    Initial draft. WIP: Missing state handling.

--- Life/ContentView.swift
+++ Life/ContentView.swift
@@ -7,12 +7,56 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CellView: View {
+
+    var row: Int!
+    var column: Int!
+
+    @State var cellState = CellState.dead
+        
+    init(row: Int, column: Int, state: CellState = .dead) {
+        self.cellState = cellState
+        self.row = row
+        self.column = column
+    }
+            
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Rectangle()
+            .fill(cellState == .alive ? Color.red : .blue)
+            .frame(width: 20, height: 20, alignment: .center)
+            .onTapGesture() {
+                cellState.toggle()
+            }
     }
 }
+
+
+struct ContentView: View {
+    
+    @ObservedObject var game = GameOfLife(rows: 10, columns: 10)
+    
+    var body: some View {
+        VStack() {
+            ForEach((1...game.rows), id: \.self) { row in
+                HStack() {
+                    ForEach((1...game.columns), id: \.self) { column in
+                        CellView(row: row, column: column, state: .dead)
+                            .padding(0)
+                    }
+                }
+                .padding(0)
+            }
+            
+            Button(action: {
+                game.start()
+            }) {
+                Text("GO")
+                    .padding(40)
+            }
+        }.padding(0)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Life/GameOfLife.swift
@@ -0,0 +1,118 @@
+//
+//  GameOfLife.swift
+//  Life
+//
+//  Created by sean on 29.11.20.
+//
+
+import Foundation
+
+
+public enum CellState {
+    
+    case alive
+    case dead
+    
+    var toggled: CellState {
+
+        switch self {
+        case .alive:
+            return .dead
+        case .dead:
+            return .alive
+        }
+    }
+    
+    mutating func toggle() {
+        self = self.toggled
+    }
+}
+
+
+class GameOfLife: ObservableObject {
+
+    let rows: Int!, columns: Int!
+    
+    private var grid: [[CellState]]!
+    private var gridBuffer: [[CellState]]!
+        
+    private var looper: Util.Looper!
+    
+
+    init(rows: Int, columns: Int) {
+        
+        self.rows = rows
+        self.columns = columns
+
+        grid = Array(repeating: Array(repeating: .dead, count: columns), count: rows)
+        gridBuffer = Array(repeating: Array(repeating: .dead, count: columns), count: rows)
+
+        looper = Util.Looper(loopTimeMillis: 500) {
+            for row in 0..<rows {
+                for column in 0..<columns {
+                    self.grid[row][column] = self.nextState(row: row, column: column)
+                }
+            }
+        }
+    }
+    
+    
+    private func nextState(row: Int, column: Int) -> CellState {
+        
+        let cellState = grid[row][column]
+        let numAlive = numNeighborsAlive(row: row, column: column)
+        
+        if cellState == .alive && [2, 3].contains(numAlive) {
+            return .alive
+        }
+        
+        if cellState == .dead && numAlive == 3 {
+            return .alive
+        }
+        
+        return .dead
+    }
+    
+    
+    private func numNeighborsAlive(row: Int, column: Int) -> Int {
+        
+        struct Neighborhood {
+            static let indices: [(Int, Int)] = [(-1, -1), (0, -1), (1, -1),
+                                                (-1,  0),          (1,  0),
+                                                (-1,  1), (0,  1), (1,  1)]
+
+        }
+        
+        let neighbors: [(Int, Int)] = Neighborhood.indices.map { (rowOffset, columnOffset) in
+            let r = (rows + row + rowOffset) % rows
+            let c = (columns + column + columnOffset) % columns
+            return (r, c)
+        }
+        
+        let aliveNeighbors = neighbors.filter { (row, column) in
+            grid[row][column] == .alive
+        }
+        
+        return aliveNeighbors.count
+    }
+    
+    
+    subscript(row: Int, column: Int) -> CellState {
+        get {
+            return grid[row][column]
+        }
+        set {
+            grid[row][column] = newValue
+        }
+    }
+
+    
+    func start() {
+        looper.resume()
+    }
+    
+    
+    func stop() {
+        looper.pause()
+    }
+}
