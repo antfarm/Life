0548185789ad998a11dd421d0542406c7efab781
Sean Buttinger <sean@antfarm.de>
2025-09-11 11:41:00 +0200

    Use .ignoresSafeArea instead of deprecated .edgesIgnoringSafeArea modifier.

--- Life/ContentView.swift
+++ Life/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
     var body: some View {
         
         ZStack() {
-            Color.black.edgesIgnoringSafeArea(.all)
+            Color.black.ignoresSafeArea()
             
             VStack() {
 
